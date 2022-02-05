411e44e4101567e76bf752a1f57c3be6708894b7
realkeny <kenny.adetutu@gmail.com>
2022-02-05 00:18:54 -0500

    added new files

---
+++ module/vpc/variables.tf
@@ -0,0 +1,29 @@
+variable "environ" {
+  type        = string
+  description = "infrastructure environment"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "The IP range to use for the VPC"
+}
+
+variable "av_zones" {
+  type = list(string)
+  description = "AZs to create subnets into"
+}
+
+variable "public_subnets" {
+  type = list(string)
+  description = "subnets to create for public network traffic, one per AZ"
+}
+
+variable "private_subnets" {
+  type = list(string)
+  description = "subnets to create for private network traffic, one per AZ"
+}
+
+variable "database_subnets" {
+  type = list(string)
+  description = "subnets to create for database traffic, one per AZ"
+}
